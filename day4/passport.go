75f15a1c51fe74076253b502a2f09f342ed8dcfa
Stephen Rowles <stephen.rowles@infosum.com>
2020-12-04 13:49:46 +0000

    less verbose!

--- day4/passport.go
+++ day4/passport.go
@@ -11,26 +11,21 @@ import (
 // Passport is a type alias for map of field to value
 type Passport map[string]string
 
+var validFields = map[string]int{
+	"byr": 1,
+	"iyr": 1,
+	"eyr": 1,
+	"hgt": 1,
+	"hcl": 1,
+	"ecl": 1,
+	"pid": 1,
+}
+
 // Valid checks if a passport is valid, with optional detailed check
 func (p Passport) Valid(detailed bool) bool {
 	count := 0
 	for key := range p {
-		switch key {
-		case "byr":
-			count++
-		case "iyr":
-			count++
-		case "eyr":
-			count++
-		case "hgt":
-			count++
-		case "hcl":
-			count++
-		case "ecl":
-			count++
-		case "pid":
-			count++
-		}
+		count += validFields[key]
 	}
 	if count != 7 {
 		return false
@@ -43,38 +38,26 @@ func (p Passport) Valid(detailed bool) bool {
 	return true
 }
 
+var validators = map[string]func(string) bool{
+	"byr": validByr,
+	"iyr": validIyr,
+	"eyr": validEyr,
+	"hgt": validHgt,
+	"hcl": validHcl,
+	"ecl": validEcl,
+	"pid": validPid,
+}
+
 // validate the fields in detail
 func (p Passport) detailed() bool {
 	for key, val := range p {
-		switch key {
-		case "byr":
-			if !validByr(val) {
-				return false
-			}
-		case "iyr":
-			if !validIyr(val) {
-				return false
-			}
-		case "eyr":
-			if !validEyr(val) {
-				return false
-			}
-		case "hgt":
-			if !validHgt(val) {
-				return false
-			}
-		case "hcl":
-			if !validHcl(val) {
-				return false
-			}
-		case "ecl":
-			if !validEcl(val) {
-				return false
-			}
-		case "pid":
-			if !validPid(val) {
-				return false
-			}
+		fn := validators[key]
+		if fn == nil {
+			continue
+		}
+
+		if !fn(val) {
+			return false
 		}
 	}
 
@@ -83,38 +66,20 @@ func (p Passport) detailed() bool {
 
 // byr (Birth Year) - four digits; at least 1920 and at most 2002.
 func validByr(val string) bool {
-	if len(val) != 4 {
-		return false
-	}
-
-	num, err := strconv.Atoi(val)
-	if err != nil {
-		return false
-	}
-	if num < 1920 || num > 2002 {
-		return false
-	}
-	return true
+	return validRange(val, 1920, 2002)
 }
 
 // iyr (Issue Year) - four digits; at least 2010 and at most 2020.
 func validIyr(val string) bool {
-	if len(val) != 4 {
-		return false
-	}
-
-	num, err := strconv.Atoi(val)
-	if err != nil {
-		return false
-	}
-	if num < 2010 || num > 2020 {
-		return false
-	}
-	return true
+	return validRange(val, 2010, 2020)
 }
 
 // eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
 func validEyr(val string) bool {
+	return validRange(val, 2020, 2030)
+}
+
+func validRange(val string, min, max int) bool {
 	if len(val) != 4 {
 		return false
 	}
@@ -123,7 +88,7 @@ func validEyr(val string) bool {
 	if err != nil {
 		return false
 	}
-	if num < 2020 || num > 2030 {
+	if num < min || num > max {
 		return false
 	}
 	return true
@@ -179,26 +144,19 @@ func validHcl(val string) bool {
 	return true
 }
 
+var validEclValues = map[string]bool{
+	"amb": true,
+	"blu": true,
+	"brn": true,
+	"gry": true,
+	"grn": true,
+	"hzl": true,
+	"oth": true,
+}
+
 // ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
 func validEcl(val string) bool {
-	switch val {
-	case "amb":
-		return true
-	case "blu":
-		return true
-	case "brn":
-		return true
-	case "gry":
-		return true
-	case "grn":
-		return true
-	case "hzl":
-		return true
-	case "oth":
-		return true
-	}
-
-	return false
+	return validEclValues[val]
 }
 
 // (Passport ID) - a nine-digit number, including leading zeroes
--- day4/passport_test.go
+++ day4/passport_test.go
@@ -75,5 +75,5 @@ iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
 
 func TestPart2(t *testing.T) {
 	passports := parse(common.ReaderFromFile("input.txt"))
-	assert.Equal(t, 260, passports.ValidCount(true))
+	assert.Equal(t, 153, passports.ValidCount(true))
 }
